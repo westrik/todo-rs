380c7b095c0ab57785e856d7052fd79d577b1713
Matthew Westrik <m@ttwestrik.com>
2019-07-16 23:54:16 -0700

    add clippy to pre-push

---
+++ src/bin/task_manager.rs
@@ -0,0 +1,30 @@
+extern crate diesel;
+extern crate todo_boilerplate;
+
+use self::diesel::prelude::*;
+use self::models::*;
+use self::todo_boilerplate::*;
+use std::env;
+
+fn main() {
+    //use todo_boilerplate::schema::tasks::dsl::*;
+
+    let args: Vec<String> = env::args().collect();
+
+    let subcommand = &args[1];
+    println!("subcommand: {}", subcommand);
+
+    let connection = establish_connection();
+
+    /*
+    let results = tasks.limit(5)
+        .load::<Task>(&connection)
+        .expect("Error loading tasks");
+
+    println!("Displaying {} tasks", results.len());
+    for task in results {
+        println!("{}", task.description);
+        println!("----------\n");
+    }
+    */
+}
