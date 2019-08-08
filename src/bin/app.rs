94a12164cda9462833a2dfc42a1aa26345e0cd5f
Matthew Westrik <m@ttwestrik.com>
2019-08-07 17:40:13 -0700

    add cors allow * header

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -5,6 +5,8 @@ extern crate rocket_contrib;
 extern crate todo_api_rs;
 
 use dotenv::dotenv;
+use rocket::fairing::AdHoc;
+use rocket::http::Header;
 use todo_api_rs::db::PgConn;
 use todo_api_rs::*; // import routes
 
@@ -12,6 +14,9 @@ fn main() {
     dotenv().ok();
     rocket::ignite()
         .attach(PgConn::fairing())
+        .attach(AdHoc::on_response("CORS header", |_, res| {
+            res.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        }))
         .mount("/api/1/", routes![list_tasks, add_task, update_task])
         .launch();
 }
